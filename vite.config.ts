36169014b410e1a1d66cf42d3c5bece5976f2f07
Marcus Gajo <musou_saber@proton.me>
2025-09-12 00:37:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    lib: {
+      entry: 'index.html',
+      formats: ['cjs', 'es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
   plugins: [react()],
   base:'/main-portfolio/'
 })
