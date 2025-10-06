b72e41c9b7da0b9e339f2b3a0975f6cab0061af4
Marcus Gajo <musou_saber@proton.me>
2025-10-07 00:09:02 +0800

    Command parser made async
    - command parser made async to support outside API calls

--- src/components/inputArea/index.tsx
+++ src/components/inputArea/index.tsx
@@ -1,14 +1,14 @@
-import { useRef, useEffect, useState, type RefObject} from 'react';
-import {initCommand } from '../../utils/commands';
+import { useRef, useEffect, useState, type RefObject } from 'react';
+import { initCommand } from '../../utils/commands';
 import './index.css'
 import { caretFill } from '../../utils/caret';
 
 
 export const TerminalInput = (props: any) => {
-    const {changeProfile, clearHistory, ctrl, ctrlCheck, shift, holdCheck, pos, recall, invokeHistory, addToHistory, onKeyDown, updateBuffer, children, ref} = props;
+    const { changeProfile, clearHistory, ctrl, ctrlCheck, shift, holdCheck, pos, recall, invokeHistory, addToHistory, onKeyDown, updateBuffer, children, ref } = props;
     const [shiftReleased, stateChange] = useState(false);
     const [ctrlReleased, ctrlChange] = useState(false);
- 
+
 
     useEffect(() => {
         if (!shift) {
@@ -18,7 +18,7 @@ export const TerminalInput = (props: any) => {
         if (shift) {
             stateChange(false)
         }
-        
+
     }, [shift])
 
     useEffect(() => {
@@ -29,7 +29,7 @@ export const TerminalInput = (props: any) => {
         if (ctrl) {
             ctrlChange(false)
         }
-        
+
     }, [ctrl])
 
     function handleKeyUp(event: any) {
@@ -38,13 +38,13 @@ export const TerminalInput = (props: any) => {
             return;
         }
 
-         if (event.key === "Control") {
+        if (event.key === "Control") {
             ctrlCheck(false);
             return;
         }
     }
 
-    function handleKeyDown(event: any) {
+    async function handleKeyDown(event: any) {
 
 
         if (event.key === "Shift") {
@@ -82,7 +82,7 @@ export const TerminalInput = (props: any) => {
                 updateBuffer(event.target.value, [ref.current!.selectionStart, ref.current!.selectionEnd + 1]);
                 return
             }
-            if ((shiftReleased || ctrlReleased) && (ref.current!.selectionEnd - ref.current!.selectionStart != 0) ) {
+            if ((shiftReleased || ctrlReleased) && (ref.current!.selectionEnd - ref.current!.selectionStart != 0)) {
                 updateBuffer(event.target.value, [ref.current!.selectionEnd, ref.current!.selectionEnd]);
                 stateChange(false)
                 ctrlChange(false)
@@ -107,34 +107,41 @@ export const TerminalInput = (props: any) => {
         if (event.key === 'Enter') {
             const value = ref.current?.value;
             onKeyDown();
-            if (typeof addToHistory == 'function'){
-                const command = initCommand(value);
-                if (typeof command == 'object' && command[0] === true) {
-                    changeProfile(command[1])
-                    addToHistory(command[2])
-                }
-                else if (command) {
-                    addToHistory(command);
-                }
-                else {
-                    clearHistory();
-                }
-           
+            if (typeof addToHistory == 'function') {
+                initCommand(value).then((command) => {
+
+                    if (typeof command == 'object' && command[0] === true) {
+                        changeProfile(command[1])
+                        addToHistory(command[2])
+                    }
+                    else if (command) {
+                        addToHistory(command);
+                    }
+                    else {
+                        clearHistory();
+                    }
+
+                }).finally(() => {
+
+                    ref.current!.value = "";
+                    recall("reset");
+                    // // clear after enter
+                    updateBuffer("", [ref.current!.selectionStart, ref.current!.selectionEnd]);
+                    invokeHistory(value);
+                    return;
+
+                });
+
             }
 
-            ref.current!.value = ""; 
-            recall("reset");
-            // // clear after enter
-            updateBuffer("", [ref.current!.selectionStart, ref.current!.selectionEnd]);
-            invokeHistory(value);
-            return;
-            
+
+
         }
-        
+
     }
 
     useEffect(() => {
-        ref.current!.value = pos; 
+        ref.current!.value = pos;
         ref.current!.focus();
         // ref.current!.setSelectionRange(ref.current!.selectionStart, ref.current!.selectionStart);
         updateBuffer(ref.current!.value, [ref.current!.selectionStart, ref.current!.selectionEnd]);
@@ -147,32 +154,32 @@ export const TerminalInput = (props: any) => {
     return (
         <div className="input-container">
             <input ref={ref} className="input-terminal" type='text' onKeyUp={handleKeyUp} onKeyDown={handleKeyDown} onChange={handleChange} />
-             {children}
+            {children}
         </div>
     )
 }
 
 export const TextDisplay = (props: any) => {
-    const inputRef : RefObject<HTMLInputElement | null> = useRef<HTMLInputElement>
-    (null);
+    const inputRef: RefObject<HTMLInputElement | null> = useRef<HTMLInputElement>
+        (null);
 
-    const {changeProfile, clearHistory, onKeyDown, addToHistory, invokeHistory, recall, pos} = props;
-    const [buffer, updateBuffer] = useState<any|null>(null);
-    const [caretpos, changecaret] = useState<any|null>([0,0]);
+    const { changeProfile, clearHistory, onKeyDown, addToHistory, invokeHistory, recall, pos } = props;
+    const [buffer, updateBuffer] = useState<any | null>(null);
+    const [caretpos, changecaret] = useState<any | null>([0, 0]);
     const [shift, holdCheck] = useState(false)
     const [ctrl, ctrlCheck] = useState(false)
 
     useEffect(() => {
         inputRef.current?.focus();
-        
+
     }, [])
 
     useEffect(() => {
-        changecaret((e:any) => [...e]);
-        
+        changecaret((e: any) => [...e]);
+
     }, [shift])
 
-    function caretChange(word: string, f:any) {
+    function caretChange(word: string, f: any) {
         updateBuffer(word);
         changecaret(() => [...f, word]);
         // updateBuffer(word);
@@ -185,24 +192,24 @@ export const TextDisplay = (props: any) => {
 
     return (
         <div className='input-sub'>
-            <TerminalInput 
-            changeProfile={(e:any) => {changeProfile(e)}}
-            pos={pos}
-            shift={shift}
-            ctrl={ctrl}
-            clearHistory={() => clearHistory()}
-            holdCheck={(e:any) => holdCheck(e)}
-            ctrlCheck={(e:any) => ctrlCheck(e)}
-            addToHistory={(e:any) => {addToHistory(e)}} 
-            invokeHistory={(e:any) => {invokeHistory(e)}} 
-            recall = {(e:string) => {recall(e)}}
-            ref={inputRef}  
-            updateBuffer={(e:string, f:any) => {caretChange(e,f)}}
-            onKeyDown={() => {onKeyDown()}}>
+            <TerminalInput
+                changeProfile={(e: any) => { changeProfile(e) }}
+                pos={pos}
+                shift={shift}
+                ctrl={ctrl}
+                clearHistory={() => clearHistory()}
+                holdCheck={(e: any) => holdCheck(e)}
+                ctrlCheck={(e: any) => ctrlCheck(e)}
+                addToHistory={(e: any) => { addToHistory(e) }}
+                invokeHistory={(e: any) => { invokeHistory(e) }}
+                recall={(e: string) => { recall(e) }}
+                ref={inputRef}
+                updateBuffer={(e: string, f: any) => { caretChange(e, f) }}
+                onKeyDown={() => { onKeyDown() }}>
                 {/* <div className="char-disp"><span style={{whiteSpace:'nowrap'}}>marcus@main-portfolio&gt;&nbsp;</span>{buffer != undefined ? caretFill(caretpos[0], caretpos[1], buffer) : caretFill(0, 1, ' ')}</div> */}
-                <div className="char-disp"><span style={{whiteSpace:'nowrap'}}>$ &gt;&nbsp;</span>{buffer != undefined ? caretFill(caretpos[0], caretpos[1], buffer) : caretFill(0, 1, ' ')}</div>
+                <div className="char-disp"><span style={{ whiteSpace: 'nowrap' }}>$ &gt;&nbsp;</span>{buffer != undefined ? caretFill(caretpos[0], caretpos[1], buffer) : caretFill(0, 1, ' ')}</div>
             </TerminalInput>
-            
+
         </div>
     )
 }
--- src/utils/commands/index.ts
+++ src/utils/commands/index.ts
@@ -1,77 +1,141 @@
 import { AppendToHistory } from '../appendToHistory';
 import { changeIdent } from '../profile-change/index.tsx';
-import {CommandList, SetupUrl, Version} from './helpCommand.tsx'
-import {getDateTime} from './chrono.tsx'
+import { CommandList, SetupUrl, Version } from './helpCommand.tsx'
+import { getDateTime } from './chrono.tsx'
+// import { resolve } from 'path';
 
 export const textparser = (text: string): string[] => {
 	const items = text.split(' ');
-	return items; 
-} 
+	return items;
+}
 
-export const commandParser_Proto =  async (text: string[]) : Promise<any> => {
+export const commandParser_Proto = async (text: string[]): Promise<any> => {
 	return new Promise((resolve, reject) => {
-		
+		const rootProgram = text[0]
+		if (rootProgram === "linkedin") {
+			resolve(SetupUrl('https://www.linkedin.com/in/marcus-luis-s-gajo-29462b212/'));
+			return;
+		}
+
+		if (rootProgram === "github") {
+			resolve(SetupUrl('https://github.com/gCookiez'));
+			return;
+		}
+
+		if (rootProgram === "portfolio") {
+			resolve(SetupUrl('/main-portfolio/portfolio.pdf'));
+			return;
+		}
+
+		if (rootProgram === "help") {
+			resolve(AppendToHistory(CommandList()));
+			return;
+		}
+
+		if (rootProgram === 'clear') {
+			resolve(false);
+			return;
+		}
+
+		if (rootProgram === 'profile') {
+
+			const result = changeIdent(text.slice(1));
+			if (!result[0]) {
+				resolve(AppendToHistory(`Error: ${result[1]}`, 'error'));
+				return;
+			}
+
+			resolve(result)
+			return;
+
+		}
+
+		if (rootProgram === 'weather') {
+			resolve(AppendToHistory('Coming soon'));
+			return;
+		}
+
+		if (rootProgram === 'datetime') {
+			resolve(AppendToHistory(getDateTime()));
+			return;
+		}
+
+		if (rootProgram === "version") {
+			resolve(AppendToHistory(Version()));
+			return;
+		}
+
+		let error = Boolean(text[0].trim()) ? rootProgram.length <= 20 ? text[0] : `${rootProgram.substring(0, 20)}...` : 'EMPTY_STRING';
+
+		resolve(AppendToHistory(`Error: ${error} is not a known command`, 'error'));
 
 	})
 }
 
 /* make this async for better callback on api calls*/
-export const commandParser = (text: string[]): any => {
-	const rootProgram = text[0]
-	if (rootProgram === "linkedin") {
-		return SetupUrl('https://www.linkedin.com/in/marcus-luis-s-gajo-29462b212/');
-	}
+// export const commandParser = (text: string[]): any => {
+// 	const rootProgram = text[0]
+// 	if (rootProgram === "linkedin") {
+// 		return SetupUrl('https://www.linkedin.com/in/marcus-luis-s-gajo-29462b212/');
+// 	}
+
+// 	if (rootProgram === "github") {
+// 		return SetupUrl('https://github.com/gCookiez');
+// 	}
+
+// 	if (rootProgram === "portfolio") {
+// 		return SetupUrl('/main-portfolio/portfolio.pdf');
+// 	}
+
+// 	if (rootProgram === "help") {
+// 		return AppendToHistory(CommandList())
+// 	}
+
+// 	if (rootProgram === 'clear') {
+// 		return false
+// 	}
+
+// 	if (rootProgram === 'profile') {
+
+// 		const result = changeIdent(text.slice(1));
+// 		if (!result[0]) {
+// 			return AppendToHistory(`Error: ${result[1]}`, 'error');
+// 		}
+
+// 		return result;
+
+
+// 	}
+
+// 	if (rootProgram === 'weather') {
+// 		return AppendToHistory('Coming soon');
+// 	}
+
+// 	if (rootProgram === 'datetime') {
+// 		return AppendToHistory(getDateTime())
+// 	}
+
+// 	if (rootProgram === "version") {
+// 		return AppendToHistory(Version());
+// 	}
+
+// 	let error = Boolean(text[0].trim()) ? rootProgram.length <= 20 ? text[0] : `${rootProgram.substring(0, 20)}...` : 'EMPTY_STRING';
+
+// 	return AppendToHistory(`Error: ${error} is not a known command`, 'error');
+// }
+
+export const initCommand = async (text: string): Promise<any> => {
+
+	return new Promise((resolve, reject) => {
+		const parsed: string[] = textparser(text);
+	// const response: any = commandParser(parsed);
+		const response: any = commandParser_Proto(parsed).then((item) => {return item});
+		resolve(response);
+	})
 	
-	if (rootProgram === "github") {
-		return SetupUrl('https://github.com/gCookiez');
-	}
-
-	if (rootProgram === "portfolio") {
-		return SetupUrl('/main-portfolio/portfolio.pdf');
-	}
-
-	if (rootProgram === "help") {
-		return AppendToHistory( CommandList() )
-	}
-
-	if (rootProgram === 'clear') {
-		return false
-	}
-
-	if (rootProgram === 'profile') {
-
-		const result = changeIdent(text.slice(1));
-		if (!result[0]) {
-			return AppendToHistory(`Error: ${result[1]}` , 'error');
-		}
-
-		return result;
-		
-
-	}
-
-	if (rootProgram === 'weather') {
-		return AppendToHistory('Coming soon');
-	}
-
-	if (rootProgram === 'datetime') {
-		return AppendToHistory(getDateTime())
-	}
-
-	if (rootProgram === "version") {
-		return AppendToHistory( Version() );
-	}
-
-	let error = Boolean(text[0].trim()) ? rootProgram.length <= 20 ? text[0] : `${rootProgram.substring(0, 20)}...` : 'EMPTY_STRING';
-
-	return AppendToHistory(`Error: ${error} is not a known command` , 'error');
+	
+	// return response;
 }
-
-export const initCommand = (text: string):any => {
-	const parsed : string[] = textparser(text);
-	const response : any = commandParser(parsed);
-	return response;
-} 
 
 
 export * from './'
